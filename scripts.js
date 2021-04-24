80e24fa165d0cfe23c340aedbf8119645416601e
Sujay Halder <32979854+know120@users.noreply.github.com>
2021-04-25 00:22:29 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ let loadImage = (src,callback) => {
 };
 
 let imagePath = (frameNumber,animation) => {
-    return "https://know120.github.io/images/" + animation + "/" + frameNumber + ".png";
+    return "https://know120.github.io/images/" + animation + "/" + frameNumber + ".png?raw = true";
 
 };
 
