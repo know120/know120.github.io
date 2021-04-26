ac409fd7d74310c6e6260308dd0b153bc7b56f64
Sujay Halder <32979854+know120@users.noreply.github.com>
2021-04-26 23:29:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 let c = document.getElementById("my-canvas");
 let ctx = c.getContext("2d");
 
+// loadImage takes image path and load image present in that path
 let loadImage = (src,callback) => {
     let img = document.createElement("img");
     img.onload = () => callback(img);
     img.src = src;
 };
 
+// imagePath generates the path for an animation
 let imagePath = (frameNumber,animation) => {
     return "https://know120.github.io/images/" + animation + "/" + frameNumber + ".png";
 
@@ -21,6 +23,7 @@ let frames = {
     backward: [1,2,3,4,5,6],
 };
 
+// load all the images for an particular animation spacified
 let loadImages = (callback) => {
     let images = {idle:[],kick:[],punch:[],block:[],forward:[],backward:[]};
     let imagesToLoad = 0;
@@ -44,6 +47,7 @@ let loadImages = (callback) => {
     });
 };
 
+// creates animation of a move
 let animate = (ctx,images,animation,callback) => {
     images[animation].forEach((image,index) => {
         setTimeout(() =>{
@@ -71,6 +75,7 @@ loadImages((images)=> {
 
     aux();
 
+    // detects clicks on page
     document.getElementById("kick").onclick = () => {
         queuedAnimations.push("kick");
         console.log("Kicked");
@@ -96,20 +101,32 @@ loadImages((images)=> {
         console.log("Coming Backward");
     };
 
+    // detects key on board
     document.addEventListener("keyup",(event) =>{
         const key = event.key;
         if (key === "ArrowLeft"){
             queuedAnimations.push("kick");
         } else if (key === "ArrowRight"){
             queuedAnimations.push("punch");
+        }else if (key === "ArrowUp"){
+            queuedAnimations.push("block");
+        }else if (key === "a" || key === "A"){
+            queuedAnimations.push("forward");
+        }else if (key === "d" || key === "D"){
+            queuedAnimations.push("backward");
         }
     });
 });
 
 
+// When the user clicks on the button, scroll to the top of the document
+let topFunction = () =>{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 
-
+/*
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -117,16 +134,12 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = () => {scrollFunction()};
 
 let scrollFunction = () =>{
-  if (document.body.scrollTop > 20 || document.getElementById("myBtn").scrollTop > 20) {
+    //if (document.body.scrollTop > 20 || document.getElementById("myBtn").scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
-    document.getElementById("myBtn").style.display = "none";
-    //mybutton.style.display = "none";
+    //document.getElementById("myBtn").style.display = "none";
+    mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-let topFunction = () =>{
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+*/
