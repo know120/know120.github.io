b01c4bca0eda57c85d65bde99aacb5c22d323c55
know120 <knowsujay@gmail.com>
2023-05-30 21:17:52 +0530

    test run after upgrate to angular 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from "./home/dashboard/dashboard.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true
+    standalone: true,
+    imports: [DashboardComponent]
 })
 export class AppComponent {
   title = 'Sujay Halder';
