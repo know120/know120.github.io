c0b2ac853e6b9eef586c0b2bdabb1a94d37c0066
know120 <knowsujay@gmail.com>
2025-09-07 12:15:29 +0530

    added tools menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import { HelmetProvider } from 'react-helmet-async';
 import Dashboard from './pages/Dashboard';
 import Note from './pages/Note';
 import AdLibrary from './pages/AdLibrary';
+import Tools from './pages/Tools';
 import './App.css';
 
 
 // Lazy load other components for better performance
 const SuperApp = React.lazy(() => import('./pages/SuperApp'));
+const Design = React.lazy(() => import('./pages/Design'));
 
 function App() {
   return (
@@ -25,9 +27,11 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/home" element={<Dashboard />} />
-              <Route path="/super" element={<SuperApp />} />
-              <Route path="/note" element={<Note />} />
-              <Route path="/ad-library" element={<AdLibrary />} />
+              <Route path="/tools" element={<Tools />} />
+              <Route path="/tools/design" element={<Design />} />
+              <Route path="/tools/super" element={<SuperApp />} />
+              <Route path="/tools/note" element={<Note />} />
+              <Route path="/tools/ad-library" element={<AdLibrary />} />
               <Route path="*" element={<Dashboard />} />
             </Routes>
           </Suspense>
