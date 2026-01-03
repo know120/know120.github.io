42cab45b9a5bfe50719054a172fb3f5ea92745a2
know120 <knowsujay@gmail.com>
2026-01-03 22:26:08 +0530

    removed unused code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,3 @@
-import { useInView } from 'react-intersection-observer';
 import Navigation from '../components/Navigation';
 import HeroSection from '../components/HeroSection';
 import Experience from '../components/Experience';
@@ -8,13 +7,6 @@ import Contact from '../components/Contact'
 import VerticalLines from '../components/common/VerticalLines';
 
 const Dashboard = () => {
-
-  // Intersection Observer for animations
-  const [aboutRef, aboutInView] = useInView({ threshold: 0.1, triggerOnce: true });
-  const [projectsRef, projectsInView] = useInView({ threshold: 0.1, triggerOnce: true });
-  const [blogRef, blogInView] = useInView({ threshold: 0.1, triggerOnce: true });
-  const [contactRef, contactInView] = useInView({ threshold: 0.1, triggerOnce: true });
-
   return (
     <div className="min-h-screen bg-slate-900 text-slate-50 relative">
       {/* <VerticalLines /> */}
