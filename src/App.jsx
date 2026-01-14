d2cabaa4e40cd734d39c1f9a6b8f20c51ab24217
know120 <knowsujay@gmail.com>
2026-01-14 15:41:54 +0530

    feat: scroll to top when navigating to new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { HelmetProvider } from 'react-helmet-async';
 import Router from './router';
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [pathname]);
+
   return (
     <HelmetProvider>
       <ThemeProvider>
